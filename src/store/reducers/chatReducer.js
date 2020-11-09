1e4ff111d59b8482d4fece6acf0963e79e40a491
Alex Nweke <nwekealexchinedu@gmail.com>
2020-11-09 22:01:48 +0100

    set up the reducer for the action type of setup_socket

--- src/store/reducers/chatReducer.js
+++ src/store/reducers/chatReducer.js
@@ -7,6 +7,12 @@ const defaultState = {
 
 const chat = (state = defaultState, action) => {
 	switch (action.type) {
+		case 'SETUP_SOCKET':
+			return {
+				...state,
+				socket: action.payload
+			};
+
 		default:
 			return state;
 	}
