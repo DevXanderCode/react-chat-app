6ac45019da8f411655ee95bc06b25167f0dfcfe6
Alex Nweke <nwekealexchinedu@gmail.com>
2020-11-09 21:59:48 +0100

    dispatch setup socket action type on open socket

--- src/store/actions/chatActions.js
+++ src/store/actions/chatActions.js
@@ -1,3 +1,10 @@
 export const setupSocket = () => {
 	const socket = new WebSocket('http://localhost:8080');
+
+	socket.onopen = () => {
+		dispatch({
+			type: 'SETUP_SOCKET',
+			payload: socket
+		});
+	};
 };
