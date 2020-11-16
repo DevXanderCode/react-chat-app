b073138c3ab6337f5c61b039984fe44e74ea2f14
Alex Nweke <nwekealexchinedu@gmail.com>
2020-11-16 10:58:49 +0100

    added some styles to the  auth component

--- src/components/pages/Auth.jsx
+++ src/components/pages/Auth.jsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
+import Styled from 'style-it';
 import { connect } from 'react-redux';
 import { withRouter, Link } from 'react-router-dom';
 import * as AuthActions from '../../store/actions/authActions';
 import { Signup, Login } from '../partials';
 
 const Auth = ({ ...props }) => {
-	return <div className="auth-wrapper">{props.match.path === '/signup' ? <Signup /> : <Login />}</div>;
+	return Styled.it(
+		`.auth-wrapper{	background: #f6f5f7;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		font-family: 'Montserrat', sans-serif;
+		height: 100vh;
+		margin: -30px 0 50px;}`,
+		<div className="auth-wrapper">{props.match.path === '/signup' ? <Signup /> : <Login />}</div>
+	);
 };
 
 const mapStateToProps = (state) => ({
