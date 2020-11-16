29fed5d62e3568b3b7fd7fefd29e2a26aac41550
Alex Nweke <nwekealexchinedu@gmail.com>
2020-11-16 10:44:20 +0100

    created the material ui formik field reuseble componenta

---
+++ src/components/common/FormikField/index.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { ErrorMessage, Field } from "formik";
+import { TextField } from "@material-ui/core";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+import {makeStyles} from "@material-ui/core/styles";
+import "./FormikField.css";
+
+interface FormikFieldProps {
+  name: string;
+  label: string;
+  required?: boolean;
+  type?: string;
+  variant?: string;
+  showIcon?: string;
+  rounded?: boolean;
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+		borderRadius: '100px'
+	}
+}));
+
+const FormikField: React.FC<FormikFieldProps> = ({
+  name,
+  label,
+  required = false,
+  type = "text",
+  variant,
+  showIcon = null,
+  rounded,
+  ...props
+}) => {
+
+  let icon = null; 
+  if (showIcon){
+    if (name === "password") {
+      icon = <VisibilityIcon />;
+    } else if (name === "email") {
+      icon = <MailOutlineIcon />;
+    }
+  }
+
+  const classes= useStyles();
+  return (
+    <div className='formikField mt-3' >
+      <Field
+        required={required}
+        name={name}
+        as={TextField}
+        variant={variant}
+        autoComplete='off'
+        label={label}
+        type={type}
+        fullWidth
+        className={{root: rounded && classes.root}}
+        helperText={<ErrorMessage name={name} />}
+        InputProps={{
+          endAdornment: icon
+        }}
+      />
+    </div>
+  );
+};
+
+export default FormikField;
--- src/components/partials/Login.jsx
+++ src/components/partials/Login.jsx
@@ -1,12 +1,47 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { Formik, Form } from 'formik';
+import FormikField from '../../components/common/FormikField';
+import * as Yup from 'yup';
 import * as AuthActions from '../../store/actions/authActions';
+import Button from '@material-ui/core/Button';
 
 const Login = () => {
+	const initialValues = {
+		email: '',
+		password: ''
+	};
+
+	const validationSchema = Yup.object().shape({
+		email: Yup.string().email().required('Please your email is required in order to login'),
+		password: Yup.string().required('Password is required').min(8, 'password must be at least 8 characters long')
+	});
 	return (
 		<div className="form-wrapper">
 			<h2>hello from the login component </h2>
+			<Formik
+				initialValues={initialValues}
+				validationSchema={validationSchema}
+				onSubmit={(values, { setSubmitting }) => {
+					console.log('logging login form values: ', values);
+				}}
+			>
+				{({ values, handleSubmit }) => (
+					<Form onSubmit={handleSubmit}>
+						<FormikField name="email" label="Email" variant="outlined" showIcon={true} required />
+						<FormikField
+							name="password"
+							type="password"
+							label="Password"
+							variant="outlined"
+							showIcon={true}
+							required
+						/>
+						<Button type="submit"> Login</Button>
+					</Form>
+				)}
+			</Formik>
 		</div>
 	);
 };
